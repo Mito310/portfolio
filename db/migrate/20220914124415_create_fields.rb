9bbc535c7f948327c7d5e785f0af0f8c17c47452
mito-g <rose.lily.iris.67115@gmail.com>
2022-09-16 00:37:57 +0900

    修正

--- db/migrate/20220914124415_create_fields.rb
+++ db/migrate/20220914124415_create_fields.rb
@@ -2,7 +2,7 @@ class CreateFields < ActiveRecord::Migration[7.0]
   def change
     create_table :fields do |t|
       t.integer :subject_id
-      t.text :field__name
+      t.text :field_name
 
       t.timestamps
     end
